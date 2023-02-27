203e6334eba50c6b5a2e291d1a45ad0c10d0c5ac
saransh-khobragade <saransh98@gmail.com>
2023-02-27 21:58:18 +0530

    testing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -116,7 +117,17 @@ func main() {
 			"message": "movie deleted successfully with id: " + id,
 		})
 	})
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	value := os.Getenv("PORT")
 	fmt.Println(r.Run(":" + value))
 }
