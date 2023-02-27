3d6a703e86bdadbd745693f2d241e6de5fd13ce6
saransh-khobragade <saransh98@gmail.com>
2023-02-27 22:03:57 +0530

    DONE

--- main.go
+++ main.go
@@ -119,7 +119,7 @@ func main() {
 	})
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"https://react-app-k76t.onrender.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowMethods:     []string{"GET"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
