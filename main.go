3e72a9f9589a5b53be8eacd25b23f9e4b23e0a3f
saransh-khobragade <saransh98@gmail.com>
2023-02-27 22:03:40 +0530

    done

--- main.go
+++ main.go
@@ -118,13 +118,13 @@ func main() {
 		})
 	})
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
+		AllowOrigins:     []string{"https://react-app-k76t.onrender.com"},
 		AllowMethods:     []string{"PUT", "PATCH"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
+			return origin == "https://react-app-k76t.onrender.com"
 		},
 		MaxAge: 12 * time.Hour,
 	}))
