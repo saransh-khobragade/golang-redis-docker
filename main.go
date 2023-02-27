f91d3ae2d0fffb08b1e7b47cb43584700c65e410
saransh-khobragade <saransh98@gmail.com>
2023-02-27 20:07:02 +0530

    testing

--- main.go
+++ main.go
@@ -118,7 +118,7 @@ func main() {
 		})
 	})
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://golang-backend-85f4.onrender.com"},
+		AllowOrigins:     []string{"https://react-app-k76t.onrender.com"},
 		AllowMethods:     []string{"GET"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
